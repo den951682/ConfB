ebbcb2f98d869d66b5a0c9473600bfb3d03f7b57
Den <den951682@gmail.com>
2025-07-10 14:01:39 +0300

    add NavigationSuite

--- app/src/main/java/com/force/confbb/ui/App.kt
+++ app/src/main/java/com/force/confbb/ui/App.kt
@@ -1,9 +1,13 @@
 package com.force.confbb.ui
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.DateRange
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.Icon
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.adaptive.WindowAdaptiveInfo
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
@@ -14,8 +18,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import com.force.confbb.R
+import com.force.confbb.designsystem.MyNavigationSuiteScaffold
+
+enum class Item(
+    val icon: ImageVector,
+    val textId: Int
+) {
+    STATUS(Icons.Rounded.Info, R.string.item_status),
+    HISTORY(Icons.Rounded.DateRange, R.string.item_history),
+    SETTINGS(Icons.Rounded.Settings, R.string.item_settings),
+}
 
 @Composable
 fun App(
@@ -37,9 +52,24 @@ fun App(
         }
     }
 
-    TextButton(
-        onClick = { showDialog = true },
-    ) {
-        Text("Settings")
-    }
+    MyNavigationSuiteScaffold(
+        items = {
+            Item.entries.forEachIndexed { index, item ->
+                item(
+                    selected = index == 1,
+                    onClick = {},
+                    icon = {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = stringResource(item.textId)
+                        )
+                    },
+                    label = {
+                        Text(stringResource(item.textId))
+                    }
+                )
+            }
+        },
+        windowAdaptiveInfo = windowAdaptiveInfo
+    ) { }
 }
