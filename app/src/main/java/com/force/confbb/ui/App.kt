cc33d936f318967421f2ba955090cad2a640813d
Den <den951682@gmail.com>
2025-07-10 14:57:31 +0300

    add content box

--- app/src/main/java/com/force/confbb/ui/App.kt
+++ app/src/main/java/com/force/confbb/ui/App.kt
@@ -1,11 +1,28 @@
 package com.force.confbb.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.DateRange
 import androidx.compose.material.icons.rounded.Info
 import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.WindowAdaptiveInfo
@@ -24,14 +41,15 @@ import com.force.confbb.R
 import com.force.confbb.designsystem.MyNavigationSuiteScaffold
 
 enum class Item(
-    val icon: ImageVector,
-    val textId: Int
+    val icon: ImageVector, val textId: Int
 ) {
-    STATUS(Icons.Rounded.Info, R.string.item_status),
-    HISTORY(Icons.Rounded.DateRange, R.string.item_history),
-    SETTINGS(Icons.Rounded.Settings, R.string.item_settings),
+    STATUS(Icons.Rounded.Info, R.string.item_status), HISTORY(Icons.Rounded.DateRange, R.string.item_history), SETTINGS(
+        Icons.Rounded.Settings,
+        R.string.item_settings
+    ),
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App(
     appState: AppState,
@@ -55,21 +73,73 @@ fun App(
     MyNavigationSuiteScaffold(
         items = {
             Item.entries.forEachIndexed { index, item ->
-                item(
-                    selected = index == 1,
-                    onClick = {},
-                    icon = {
-                        Icon(
-                            imageVector = item.icon,
-                            contentDescription = stringResource(item.textId)
-                        )
-                    },
-                    label = {
-                        Text(stringResource(item.textId))
-                    }
-                )
+                item(selected = index == 1, onClick = {}, icon = {
+                    Icon(
+                        imageVector = item.icon, contentDescription = stringResource(item.textId)
+                    )
+                }, label = {
+                    Text(stringResource(item.textId))
+                })
             }
-        },
-        windowAdaptiveInfo = windowAdaptiveInfo
-    ) { }
+        }, windowAdaptiveInfo = windowAdaptiveInfo
+    ) {
+        Scaffold(
+            snackbarHost = {
+                SnackbarHost(
+                    snackbarHostState,
+                    modifier = Modifier.windowInsetsPadding(WindowInsets.safeDrawing)
+                )
+            },
+            contentWindowInsets = WindowInsets(0, 0, 0, 0),
+        ) { padding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+                    .consumeWindowInsets(padding)
+                    .windowInsetsPadding(
+                        WindowInsets.safeDrawing.only(
+                            WindowInsetsSides.Horizontal
+                        )
+                    )
+            ) {
+                val shouldShowTopAppBar = true
+                if (shouldShowTopAppBar) {
+                    CenterAlignedTopAppBar(
+                        title = { Text(stringResource(R.string.item_status)) },
+                        actions = {
+                            IconButton(
+                                onClick = { showDialog = true },
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Rounded.Add,
+                                    null,
+                                    tint = MaterialTheme.colorScheme.onSurface,
+                                )
+                            }
+                        },
+                    )
+                }
+                Box(
+                    modifier = Modifier
+                        .windowInsetsPadding(
+                            if (shouldShowTopAppBar) {
+                                WindowInsets(0, 0, 0, 0)
+                            } else {
+                                WindowInsets.safeDrawing.only(WindowInsetsSides.Top)
+                            }
+                        )
+                        .consumeWindowInsets(
+                            if (shouldShowTopAppBar) {
+                                WindowInsets.safeDrawing.only(WindowInsetsSides.Top)
+                            } else {
+                                WindowInsets(0, 0, 0, 0)
+                            }
+                        )
+                ) {
+                    Text("Example Text")
+                }
+            }
+        }
+    }
 }
