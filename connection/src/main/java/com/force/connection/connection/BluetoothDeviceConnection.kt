e103ceeb00da71144230b937e2b15f406d29a50b
Den <den951682@gmail.com>
2025-08-02 07:16:51 +0300

    DluetoothDeviceConnection

--- connection/src/main/java/com/force/connection/connection/BluetoothDeviceConnection.kt
+++ connection/src/main/java/com/force/connection/connection/BluetoothDeviceConnection.kt
@@ -5,6 +5,7 @@ import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothSocket
 import com.force.connection.CONN_TAG
 import com.force.connection.ConnectionDefaults.log
+import com.force.model.ConfException
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
@@ -12,7 +13,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.flowOf
 import java.util.UUID
 
-@SuppressLint("MissingPermission")
 class BluetoothDeviceConnection @AssistedInject constructor(
     @Assisted private val deviceAddress: String,
     @Assisted private val scope: CoroutineScope,
@@ -23,6 +23,7 @@ class BluetoothDeviceConnection @AssistedInject constructor(
     private lateinit var btSocket: BluetoothSocket
     override lateinit var socket: SocketIO
 
+    @SuppressLint("MissingPermission")
     override val info = flowOf(
         DeviceConnection.Info(
             type = DeviceConnection.Type.Bluetooth,
@@ -32,19 +33,27 @@ class BluetoothDeviceConnection @AssistedInject constructor(
     )
 
     override fun connect() {
+        if (::btSocket.isInitialized) {
+            log(CONN_TAG, "Already connected to $deviceAddress, disconnect")
+            release()
+        }
         val sppUuid: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
-        val bluetoothDevice = bluetoothManager.adapter.getRemoteDevice(deviceAddress)
-        btSocket = bluetoothDevice.createRfcommSocketToServiceRecord(sppUuid)
-        btSocket.connect()
-        socket = ConnectionSocketIO(
-            btSocket.inputStream,
-            btSocket.outputStream
-        )
-        log(CONN_TAG, "Connected to $deviceAddress")
+        try {
+            val bluetoothDevice = bluetoothManager.adapter.getRemoteDevice(deviceAddress)
+            btSocket = bluetoothDevice.createRfcommSocketToServiceRecord(sppUuid)
+            btSocket.connect()
+            socket = ConnectionSocketIO(
+                btSocket.inputStream,
+                btSocket.outputStream
+            )
+            log(CONN_TAG, "Connected to $deviceAddress")
+        } catch (ex: Exception) {
+            log(CONN_TAG, "Failed to connect to $deviceAddress: ${ex.message}")
+            throw ConfException.SocketException()
+        }
     }
 
     override fun release() {
-        runCatching { socket.close() }
         runCatching { btSocket.close() }
         super.release()
     }
